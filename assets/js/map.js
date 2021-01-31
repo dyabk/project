69ffd5decc07d189656354a4f797a8ecc97c6b99
Richard Solomon <35183675+RichieSolo@users.noreply.github.com>
2021-01-30 23:58:50 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,111 +1,242 @@
-let map, infoWindow;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 13,
-        center: new google.maps.LatLng(25.7574, -80.3733),
-        mapTypeId: "terrain",
-    });
+//Based on https://github.com/googlecodelabs/google-maps-nearby-search-js/edit/master/step4/index.html, but the code was modified.
     
-    var foodBanks = {
-        "type": "FeatureCollection",
-        "name": "Food banks",
-        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-        "features": [
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Grove Outreach", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2543261, 25.7278571 ] } },
-        { "type": "Feature", "properties": { "Name": "Glory Temple Ministries", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2338263, 25.8471552 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Saint Vincent de Paul Mother of Christ", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.4228722, 25.7440922 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Comunidad de Cristo", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.3040089, 25.7568134 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Verbo Community Outreach", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.3148667, 25.7783333 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - True Gospel", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1932034, 25.8502207 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Glory Temple Ministry", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2339176, 25.8471509 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Prayer and Praise International Ministry", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.224681, 25.830792 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Refuge Church of Our Lord", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2245286, 25.8159756 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Food For Life Network", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.189722, 25.8096488 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Miami Rescue Mission", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.197514, 25.797901 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Saint John Baptist Church", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2001904, 25.7873258 ] } },
-        { "type": "Feature", "properties": { "Name": "Hallandale Food Pantry", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1544771, 25.9818786 ] } },
-        { "type": "Feature", "properties": { "Name": "Feeding South Florida", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1735221, 25.9880838 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Jubilee Center Of South Broward", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1482585, 26.0297387 ] } },
-        { "type": "Feature", "properties": { "Name": "Liberia Economic & Social Development, Inc.", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1542776, 26.0207617 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - L.E.S.", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1555384, 26.0416978 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Davie Community Worship Center", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2187551, 26.0706101 ] } },
-        { "type": "Feature", "properties": { "Name": "iglesia Adventista Martes", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1438214, 26.1075458 ] } },
-        { "type": "Feature", "properties": { "Name": "New Hope World Outreach", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.140983, 26.141959 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Salvation Army of Broward", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.160767, 26.122326 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Cooperative Feeding", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1910403, 26.1215343 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Miami Gardens Food Pantry Inc", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.212524, 25.951202 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - North Miami Church of God", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.1998635, 25.9059224 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - North Miami New Apostolic", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2131992, 25.9013853 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - North Miami Church of Nazarene", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2185808, 25.8881908 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Stop Hunger Inc.", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.171296, 25.886861 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - South Florida Urban Ministries", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.2190794, 25.8802158 ] } },
-        { "type": "Feature", "properties": { "Name": "Miami Kosher Food Bank", "description": "The JCS Kosher Food Bank is the only food bank in Miami-Dade County to which those who observe kosher (kashrut) dietary laws can turn. JCS’ Kosher Food Bank provides food to more than 380 families and nearly 100 Holocaust survivors." }, "geometry": { "type": "Point", "coordinates": [ -80.1691726, 25.9642648 ] } },
-        { "type": "Feature", "properties": { "Name": "Food Distribution Center - Reparando Portillos", "description": null }, "geometry": { "type": "Point", "coordinates": [ -80.3225728, 26.1131056 ] } }
-        ]
+    let pos;
+    let map;
+    let bounds;
+    let infoWindow;
+    let currentInfoWindow;
+    let service;
+    let infoPane;
+    function initMap() {
+      // Initialize variables
+      bounds = new google.maps.LatLngBounds();
+      infoWindow = new google.maps.InfoWindow();
+      currentInfoWindow = infoWindow;
+      /* TODO: Step 4A3: Add a generic sidebar */
+      infoPane = document.getElementById('panel');
+
+      // Try HTML5 geolocation
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: pos,
+            zoom: 15
+          });
+          bounds.extend(pos);
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Your current location.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+
+          // Call Places Nearby Search on user's location
+          getNearbyPlaces(pos);
+        }, () => {
+          // Browser supports geolocation, but user has denied permission
+          handleLocationError(true, infoWindow);
+        });
+      } else {
+        // Browser doesn't support geolocation
+        handleLocationError(false, infoWindow);
+      }
+      const centerControlDiv = document.createElement("div");
+      const control = new CenterControl(centerControlDiv, map, pos);
+      centerControlDiv.index = 1;
+      centerControlDiv.style.paddingTop = "10px";
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+    }
+
+    // Handle a geolocation error
+    function handleLocationError(browserHasGeolocation, infoWindow) {
+      // Set default location to Miami, Florida
+      pos = { lat: 25.7617, lng: -80.1918 };
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 15
+      });
+
+      // Display an InfoWindow at the map center
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Geolocation permissions denied. Using default location.' :
+        'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+      currentInfoWindow = infoWindow;
+
+      // Call Places Nearby Search on the default location
+      getNearbyPlaces(pos);
+    }
+
+    // Perform a Places Nearby Search Request
+    function getNearbyPlaces(position) {
+      let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        keyword: "(restaurants) AND (food pantry)"
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, nearbyCallback);
     }
     
-    map.data.addGeoJson(foodBanks);
+    // Handle the results (up to 20) of the Nearby Search
+    function nearbyCallback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+      }
+    }
     
-    infoWindow = new google.maps.InfoWindow();
-    const locationButton = document.createElement("button");
-    locationButton.textContent = "Near Me";
-    locationButton.classList.add("custom-map-control-button");
-
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-    
-    locationButton.addEventListener("click", () => {
-          // Try HTML5 geolocation.
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-              (position) => {
-                const pos = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude,
-                };
-                infoWindow.setPosition(pos);
-                infoWindow.setContent("Location found.");
-                infoWindow.open(map);
-                map.setCenter(pos);
-              },
-              () => {
-                handleLocationError(true, infoWindow, map.getCenter());
-              }
-            );
-          } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-          }
+    // Set markers at the location of each place result
+    function createMarkers(places) {
+      places.forEach(place => {
+        let marker = new google.maps.Marker({
+          position: place.geometry.location,
+          map: map,
+          title: place.name
         });
-    
-    // Create a <script> tag and set the USGS URL as the source.
-    //const script = document.createElement("script");
-    // This example uses a local copy of the GeoJSON stored at
 
-    //http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    //script.src = "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
-    //document.getElementsByTagName("head")[0].appendChild(script);
-}
+        /* TODO: Step 4B: Add click listeners to the markers */
+        // Add click listener to each marker
+        google.maps.event.addListener(marker, 'click', () => {
+          let request = {
+            placeId: place.place_id,
+            fields: ['name', 'formatted_address', 'geometry', 'rating',
+              'website', 'photos']
+          };
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-        browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
-}
+          /* Only fetch the details of a place when the user clicks on a marker.
+           * If we fetch the details for all place results as soon as we get
+           * the search response, we will hit API rate limits. */
+          service.getDetails(request, (placeResult, status) => {
+            showDetails(placeResult, marker, status)
+          });
+        });
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
-    const latLng = new google.maps.LatLng(coords[1], coords[0]);
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
+        // Adjust the map bounds to include the location of this marker
+        bounds.extend(place.geometry.location);
+      });
+      /* Once all the markers have been placed, adjust the bounds of the map to
+       * show all the markers within the visible area. */
+      map.fitBounds(bounds);
+    }
+
+    /* TODO: Step 4C: Show place details in an info window */
+    // Builds an InfoWindow to display details above the marker
+    function showDetails(placeResult, marker, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        let placeInfowindow = new google.maps.InfoWindow();
+        let rating = "None";
+        if (placeResult.rating) rating = placeResult.rating;
+        placeInfowindow.setContent('<div><strong>' + placeResult.name +
+          '</strong><br>' + 'Rating: ' + rating + '</div>');
+        placeInfowindow.open(marker.map, marker);
+        currentInfoWindow.close();
+        currentInfoWindow = placeInfowindow;
+        showPanel(placeResult);
+      } else {
+        console.log('showDetails failed: ' + status);
+      }
+    }
+
+    /* TODO: Step 4D: Load place details in a sidebar */
+    // Displays place details in a sidebar
+    function showPanel(placeResult) {
+      // If infoPane is already open, close it
+      if (infoPane.classList.contains("open")) {
+        infoPane.classList.remove("open");
+      }
+
+      // Clear the previous details
+      while (infoPane.lastChild) {
+        infoPane.removeChild(infoPane.lastChild);
+      }
+
+      /* TODO: Step 4E: Display a Place Photo with the Place Details */
+      // Add the primary photo, if there is one
+      if (placeResult.photos) {
+        let firstPhoto = placeResult.photos[0];
+        let photo = document.createElement('img');
+        photo.classList.add('hero');
+        photo.src = firstPhoto.getUrl();
+        infoPane.appendChild(photo);
+      }
+
+      // Add place details with text formatting
+      let name = document.createElement('h1');
+      name.classList.add('place');
+      name.textContent = placeResult.name;
+      /*infoPane.appendChild(name);*/
+      if (placeResult.rating) {
+        let rating = document.createElement('p');
+        rating.classList.add('details');
+        rating.textContent = `Rating: ${placeResult.rating} \u272e`;
+        infoPane.appendChild(rating);
+      }
+      let address = document.createElement('p');
+      address.classList.add('details');
+      address.textContent = placeResult.formatted_address;
+      infoPane.appendChild(address);
+      if (placeResult.website) {
+        let websitePara = document.createElement('p');
+        let websiteLink = document.createElement('a');
+        let websiteUrl = document.createTextNode(placeResult.website);
+        websiteLink.appendChild(websiteUrl);
+        websiteLink.title = placeResult.website;
+        websiteLink.href = placeResult.website;
+        websitePara.appendChild(websiteLink);
+        infoPane.appendChild(websitePara);
+      }
+
+      // Open the infoPane
+      infoPane.classList.add("open");
+    }
+
+
+class CenterControl {
+  constructor(controlDiv, map, center) {
+    this.map_ = map;
+    // Set the center property upon construction
+    this.center_ = new google.maps.LatLng(center);
+    controlDiv.style.clear = "both";
+    // Set CSS for the control border
+    const goCenterUI = document.createElement("div");
+    goCenterUI.id = "goCenterUI";
+    goCenterUI.title = "Click to recenter the map";
+    controlDiv.appendChild(goCenterUI);
+    // Set CSS for the control interior
+    const goCenterText = document.createElement("div");
+    goCenterText.id = "goCenterText";
+    goCenterText.innerHTML = "Center Map";
+    goCenterUI.appendChild(goCenterText);
+    // Set CSS for the setCenter control border
+    const setCenterUI = document.createElement("div");
+    setCenterUI.id = "setCenterUI";
+    setCenterUI.title = "Click to change the center of the map";
+    controlDiv.appendChild(setCenterUI);
+    // Set CSS for the control interior
+    const setCenterText = document.createElement("div");
+    setCenterText.id = "setCenterText";
+    setCenterText.innerHTML = "Set Center";
+    setCenterUI.appendChild(setCenterText);
+    // Set up the click event listener for 'Center Map': Set the center of
+    // the map
+    // to the current center of the control.
+    goCenterUI.addEventListener("click", () => {
+      const currentCenter = this.center_;
+      this.map_.setCenter(currentCenter);
+    });
+    // Set up the click event listener for 'Set Center': Set the center of
+    // the control to the current center of the map.
+    setCenterUI.addEventListener("click", () => {
+      const newCenter = this.map_.getCenter();
+      this.center_ = newCenter;
     });
   }
-};
+}
+
+
+
